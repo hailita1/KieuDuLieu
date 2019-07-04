efefe50f5a557c2795c8c3f9637fb3c8e9b12072
= <=>
2019-07-04 11:30:12 +0700

    Kieu Du Lieu

---
+++ src/kieuDuLieu.java
@@ -0,0 +1,16 @@
+public class kieuDuLieu{
+    public static void main(String[] args) {
+        int i = 10;
+        double j = 20.5;
+        double d = 20.5;
+        boolean b = true;
+        char c = 'a';
+        String s = "Hà Nội";
+        System.out.println(i);
+        System.out.println(j);
+        System.out.println(d);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(s);
+    }
+}
